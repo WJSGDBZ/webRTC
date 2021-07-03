c5feae6191a9ffb9701dec3ef2b7160331a4e896
jun <lj419449050@163.com>
2021-07-03 15:23:27 +0800

    commit

--- device/public/peerconnection/main.js
+++ device/public/peerconnection/main.js
@@ -1,22 +1,188 @@
 'use strict'
-
+//video
 var localVideo = document.getElementById("localVideo");
 var remoteVideo = document.getElementById("remoteVideo");
 
+//chat 
+var username = document.getElementById("username");
+var inRoom = document.getElementById("room");
+var outContent = document.getElementById("chat");
+var input = document.getElementById("input");
+
+//button
+var btnSend = document.getElementById("send");
 var btnStart = document.getElementById("start");
-var btnCall = document.getElementById("call");
 var btnHangup = document.getElementById("hangup");
 
+//select
+var selbw = document.getElementById("bandwidth");
+
+//Stats
+var bitrateGraph;
+var bitrateSeries;
+
+var packetGraph;
+var packetSeries;
+
+//SDP
 var offer = document.getElementById("offer");
 var answer = document.getElementById("answer");
 
-var localStream;
-var pc1;
-var pc2;
+//param
+var roomId;
+var lastResult;
+var state = 'init';
+
+//need to close
+var localStream = null;
+var pc = null;
+var dc = null;
+var socket = null;
+
+function call(){
+	if(state === 'joined_conn'){
+		if(pc){
+			var options = {
+				//接收远端视频和音频
+				offerToReceiveAudio: 1,
+				offerToReceiveVideo: 1,
+			};
+			pc.createOffer(options)
+				.then(getOffer)
+				.catch(handleOfferError);
+		}		
+	}
+}
+
+function dataChannelStateChange(e){
+	var state = dc.readyState;
+	if(state === 'open'){
+		input.disabled = false;
+                btnSend.disabled = false;		
+	}else{
+                input.disabled = true;
+                btnSend.disabled = true;
+	}
+}
+
+function recvmsg(e){
+	//console.log('recv msg',e);
+	var msg = e.data;
+	if(msg){
+		outContent.scrollTop = outContent.scrollHeight;
+                outContent.value = outContent.value + msg + '\r';	
+	}else{
+		console.error('recv msg is null');
+	}
+}
+
+function conn(){
+	socket = io.connect();//连接信令服务器
+	
+	socket.on('joined',(room,id)=>{
+		console.log("state change:",state,'to','joined');
+		state = 'joined';
+
+		createPeerConn();
+
+                btnStart.disabled = true;
+                btnHangup.disabled = false;
+	});
+
+	socket.on('otherjoin',(room,id)=>{
+		console.log("state change:",state,'to','joined_conn');
+		if(state == 'joined_unbind'){
+			createPeerConn();
+		}
+		
+		dc = pc.createDataChannel('chat');
+		dc.onmessage = recvmsg;
+		dc.onopen = dataChannelStateChange;
+		dc.onclose = dataChannelStateChange;
+
+		state = 'joined_conn';
+		//媒体协商
+		call();	
+	});
+
+	socket.on('full',(room,id)=>{
+		console.log("state change:",state,'to','leaved');
+		state = 'leaved';
+		socket.disconnect();
+		alert("room full");
+
+                btnStart.disabled = false;
+                btnHangup.disabled = true;
+        });
+
+        socket.on('leaved',(room,id)=>{
+                console.log("state change:",state,'to','leaved');
+                state = 'leaved';
+                socket.disconnect();
+
+		btnStart.disabled = false;
+		btnHangup.disabled = true;
+        });
+
+        socket.on('bye',(room,id)=>{
+		console.log("state change:",state,'to','joined_unbind');
+		state = 'joined_unbind';
+		closePeerConn();
+
+        });
+
+	socket.on('message',(room,data)=>{
+		if(data){
+			if(data.hasOwnProperty('type') && data.type === 'offer'){
+				offer.value = data.sdp;
+				//发过来时已经是文本了，不是对象了
+				pc.setRemoteDescription(new RTCSessionDescription(data));
+				pc.createAnswer()
+					.then(getAnswer)
+					.catch(handleAnswerError);
+			}else if(data.hasOwnProperty('type') && data.type === 'answer'){
+				answer.value = data.sdp;
+				pc.setRemoteDescription(new RTCSessionDescription(data));
+				selbw.disabled = false;
+			}else if(data.hasOwnProperty('type') && data.type === 'candidate'){
+				var candidate = new RTCIceCandidate({
+					sdpMLineIndex:data.label,
+					candidate: data.candidate
+				});
+
+				pc.addIceCandidate(candidate);
+			}else{
+				console.error('the messgae is invaild',data);
+			}
+		}
+	});
+
+        //socket.on('chat',(room,data)=>{
+        //        outContent.scrollTop = outContent.scrollHeight;
+        //        outContent.value = outContent.value + data + '\r';
+
+        //});
+
+	roomId = inRoom.value;
+	console.log('join room: ',roomId);
+	socket.emit('join',roomId);
+
+
+}
 
 function getMediaStream(stream){
 	localVideo.srcObject = stream;
 	localStream = stream;
+	
+	conn();
+
+	bitrateSeries = new TimelineDataSeries();
+	bitrateGraph = new TimelineGraphView('bitrateGraph', 'bitrateCanvas');
+	bitrateGraph.updateEndDate();
+
+	packetSeries = new TimelineDataSeries();
+	packetGraph = new TimelineGraphView('packetGraph', 'packetCanvas');
+	packetGraph.updateEndDate();
 }
 
 function handleError(err){
@@ -24,11 +190,11 @@ function handleError(err){
 }
 
 function getAnswer(desc){
-	pc2.setLocalDescription(desc);
+	pc.setLocalDescription(desc);
 	answer.value = desc.sdp;
-	pc1.setRemoteDescription(desc);
 
-
+	selbw.disabled = false;
+	sendMessage(roomId,desc);
 }	
 function handleAnswerError(err){
 	console.error("fail to create answer",err);
@@ -38,16 +204,19 @@ function handleOfferError(err){
 	console.error("fail to create offer",err);
 }
 
+function sendMessage(roomId,data){
+	
+	console.log('send p2p message',roomId,data);
+	if(socket){
+		socket.emit('message',roomId,data);
+	}
+}
+
 function getOffer(desc){
-	pc1.setLocalDescription(desc);
+	pc.setLocalDescription(desc);//去收集candidate
 	offer.value = desc.sdp
 
-	pc2.setRemoteDescription(desc);
-
-	pc2.createAnswer()
-		.then(getAnswer)
-		.catch(handleAnswerError);
-
+	sendMessage(roomId,desc);
 }
 
 function start(){
@@ -64,10 +233,6 @@ function start(){
 		navigator.mediaDevices.getUserMedia(constraints)
 					.then(getMediaStream)
 					.catch(handleError);
-
-		btnStart.disabled = true;
-		btnCall.disabled = false;
-		btnHangup.disabled = true;
 	}
 }
 
@@ -75,50 +240,197 @@ function getRemoteStream(e){
 	remoteVideo.srcObject = e.streams[0];
 }
 
-function call(){
-	pc1 = new RTCPeerConnection();
-	pc2 = new RTCPeerConnection();
+function closeLocalMedia(){
+	if(localStream){
+		console.log('close LocalStream');
+		if(localStream.getTracks()){
+                	localStream.getTracks().forEach((track)=>{
+	                        track.stop();
+        	        });
+		}
+	}
 
-	pc1.onicecandidate = (e)=>{
-		pc2.addIceCandidate(e.candidate);
+	localStream = null;
+}
+
+function leave(){
+	if(socket){
+		socket.emit('leave',roomId);
+	}
+
+        closePeerConn();
+       	closeLocalMedia();
+
+	btnStart.disabled = false;
+	btnHangup.disabled = true;
+	outContent.value = "";
+}
+
+function connSignalServer(){
+	//开启本地视频
+	start();
+}
+
+function closePeerConn(){
+	if(pc){
+		console.log("close peer conn");
+		pc.close();
+		pc = null;
 	}
 	
-	pc2.onicecandidate = (e)=>{
-                pc1.addIceCandidate(e.candidate);
-        }
-
-	pc2.ontrack = getRemoteStream;
-
-	localStream.getTracks().forEach((track)=>{
-		pc1.addTrack(track,localStream);
-	});
-	var Offer0ptions = {
-		offerToReceiveAudio:1,
-		offerToReceiveVideo:1
-	};
-	pc1.createOffer(Offer0ptions)
-		.then(getOffer)
-		.catch(handleOfferError);
-
-	btnCall.disabled = true;
-	btnHangup.disabled = false;
-
 }
 
-function hangup(){
-	pc1.close();
-	pc2.close();
-	pc1 = null;
-	pc2 = null;
+function createPeerConn(){
+	if(!pc){
+		console.log('create peer conn');
+		var pcConfig = {
+			'iceServers':[{
+				'urls':'turn:116.62.104.83:3478',
+				'credential':'123',
+				'username':'jun'
+			},{
+				'url':'stun:stun.l.google.com:19302'
+			}],
+			
+		};
+		pc = new RTCPeerConnection(pcConfig);
+
+		pc.onicecandidate = (e)=>{
+			if(e.candidate){
+				console.log('find candidate',e.candidate);
+				sendMessage(roomId,{
+					type:'candidate',
+					label:e.candidate.sdpMLineIndex,
+					id:e.candidate.sdpMid,
+					candidate:e.candidate.candidate
+				});		
+			}
+		}
+
+		pc.ondatachannel = (e)=>{
+			if(!dc){
+				dc = e.channel;
+				dc.onmessage = recvmsg;
+				dc.onopen = dataChannelStateChange;
+				dc.onclose = dataChannelStateChange;
+			}
+		}
+
+		pc.ontrack = getRemoteStream;
+	}
+
+	if(localStream){
+		localStream.getTracks().forEach((track)=>{
+			pc.addTrack(track,localStream);	
+		});
+	}
+}
+
+
+function changeBw(){
+	selbw.diabled = true;
+	var bw = selbw.options[selbw.selectedIndex].value;
+	var vsender = null;
+	var senders = pc.getSenders();//获取发送器
 	
-	btnCall.disabled = false;
-	btnHangup.disabled = true;
-	offer.value = "";
-	answer.value = "";
+	senders.forEach((sender)=>{
+		if(sender && sender.track.kind == 'video'){
+			vsender = sender;
+		}
+	});
+
+	var param = vsender.getParameters();
+	if(!param.encodings){
+		return ;
+	}
+	
+
+	param.encodings[0].maxBitrate = (bw==='unlimited')?'unlimited':bw * 1024;
+	vsender.setParameters(param)
+		.then(()=>{
+			selbw.disabled = false;
+			console.log("successed to set param");
+		})
+		.catch((err)=>{
+			console.error("failed to set param",err);
+		});
+
 }
 
-btnStart.onclick = start;
-btnCall.onclick = call;
-btnHangup.onclick = hangup;
+window.setInterval(()=>{
+	if(!pc){
+		return ;
+	}
+        var vsender = null;
+        var senders = pc.getSenders();//获取发送器
 
+        senders.forEach((sender)=>{
+                if(sender && sender.track.kind == 'video'){
+                        vsender = sender;
+                }
+        });
+	if(!vsender){
+		return ;
+	}
+
+	vsender.getStats().then(res => {
+    	res.forEach(report => {
+      	let bytes;
+      	let packets;
+      	if (report.type === 'outbound-rtp') {
+        	if (report.isRemote) {
+          	return;
+        	}
+        	const now = report.timestamp;
+        	bytes = report.bytesSent;
+        	packets = report.packetsSent;
+        	if (lastResult && lastResult.has(report.id)) {
+          	// calculate bitrate
+          	const bitrate = 8 * (bytes - lastResult.get(report.id).bytesSent) /
+            	(now - lastResult.get(report.id).timestamp);
+
+          	// append to chart
+		console.log(bitrateSeries);
+          	bitrateSeries.addPoint(now, bitrate);
+          	bitrateGraph.setDataSeries([bitrateSeries]);
+          	bitrateGraph.updateEndDate();
+
+          	// calculate number of packets and append to chart
+          	packetSeries.addPoint(now, packets -
+            	lastResult.get(report.id).packetsSent);
+          	packetGraph.setDataSeries([packetSeries]);
+          	packetGraph.updateEndDate();
+        	}
+      	}
+    	});
+    	lastResult = res;
+  	});
+}, 1000);
+
+btnSend.onclick = ()=>{
+        var data = input.value;
+        data = username.value + ":" + data;
+        outContent.scrollTop = outContent.scrollHeight;
+        outContent.value = outContent.value + data + '\r';
+        //socket.emit('chat',roomId,data);
+	dc.send(data);
+        input.value = "";
+}
+
+input.onkeypress = (event)=> {
+    //event = event || window.event;
+    if (event.keyCode == 13) { //回车发送消息
+        var data = input.value;
+        data = username.value + ':' + data;
+        //socket.emit('chat', roomId, data);
+	dc.send(data);
+        outContent.scrollTop = outContent.scrollHeight;
+        outContent.value = outContent.value + data + '\r';
+        input.value = '';
+        event.preventDefault();//阻止默认行为
+    }
+}
+btnStart.onclick = connSignalServer;
+btnHangup.onclick = leave;
+selbw.onchange = changeBw;
 
--- device/web_server.js
+++ device/web_server.js
@@ -1,10 +1,31 @@
 'use strict'
 
 var http = require('http');
-
+var log4js = require('log4js');
 var express = require('express');
 var serveIndex = require('serve-index');
 var socketIO = require('socket.io');
+
+log4js.configure({
+    appenders: {
+        file: {
+            type: 'file',
+            filename: 'app.log',
+            layout: {
+                type: 'pattern',
+                pattern: '%r %p - %m',
+            }
+        }
+    },
+    categories: {
+       default: {
+          appenders: ['file'],
+          level: 'debug'
+       }
+    }
+});
+
+var logger = log4js.getLogger();
 
 var app = express();
 app.use(serveIndex('./public'));
@@ -14,19 +35,34 @@ var http_server = http.createServer(app);
 //bind socket.io with http_server
 var io = socketIO(http_server);
 
+var USER_MAX_NUMBER = 2;
 //connection
 io.sockets.on('connection', (socket)=>{
 
 	socket.on('message', (room, data)=>{
-		socket.to(room).emit('message', room, socket.id, data)//房间内所有人
+		socket.to(room).emit('message', room, data)//房间内除自己所有人
 	});
+
+        socket.on('chat', (room, data)=>{
+                socket.to(room).emit('chat', room, data)//房间内除自己所有人
+        });
 
 	socket.on('join', (room)=> {
 		socket.join(room);
-		var myRoom = io.sockets.adapter.rooms[room];
-		//var users = Object.keys(myRoom.sockets).length;
-	
-	 	socket.emit('joined', room, socket.id);
+		var myRoom = io.sockets.adapter.rooms.get(room);
+		var users = (myRoom)?myRoom.size:0;
+		logger.debug('the user number of room is: ' + users);
+
+		if(users <= USER_MAX_NUMBER){
+			socket.emit('joined', room, socket.id);	
+			if(users > 1){
+				socket.to(room).emit('otherjoin',room,socket.id);
+			}
+		}else{
+			socket.leave(room);
+			socket.emit('full',room,socket.id);
+		}
+	 	
 	 	//socket.to(room).emit('joined', room, socket.id);//除自己之外
 		//io.in(room).emit('joined', room, socket.id)//房间内所有人
 	 	//socket.broadcast.emit('joined', room, socket.id);//除自己，全部站点
@@ -38,7 +74,8 @@ io.sockets.on('connection', (socket)=>{
 		//users - 1;
 
 		socket.leave(room);
-	 	socket.emit('leaved', room, socket.id);
+	 	socket.to(room).emit('bye', room, socket.id);
+		socket.emit('leaved',room,socket.id);
 	 	//socket.to(room).emit('joined', room, socket.id);//除自己之外
 		//io.in(room).emit('joined', room, socket.id)//房间内所有人
 	 	//socket.broadcast.emit('joined', room, socket.id);//除自己，全部站点
