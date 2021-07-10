89e35cc6543d9f768a1a49f5fcf74f5948a9e9d5
jun <lj419449050@163.com>
2021-07-10 15:38:09 +0800

    support https

--- device/web_server.js
+++ device/web_server.js
@@ -6,6 +6,9 @@ var express = require('express');
 var serveIndex = require('serve-index');
 var socketIO = require('socket.io');
 
+var https = require('https');
+var fs = require('fs');
+
 log4js.configure({
     appenders: {
         file: {
@@ -31,9 +34,18 @@ var app = express();
 app.use(serveIndex('./public'));
 app.use(express.static('./public'));
 
-var http_server = http.createServer(app);
+//var http_server = http.createServer(app);
 //bind socket.io with http_server
-var io = socketIO(http_server);
+//var io = socketIO(http_server);
+
+var options = {
+	key : fs.readFileSync('./cert/mylearning.shop.key'),
+	cert: fs.readFileSync('./cert/mylearning.shop.pem')
+}
+
+//https server
+var https_server = https.createServer(options, app);
+var io = socketIO(https_server);
 
 var USER_MAX_NUMBER = 2;
 //connection
@@ -82,4 +94,4 @@ io.sockets.on('connection', (socket)=>{
 	});
 });
 
-http_server.listen(80,'0.0.0.0');
+https_server.listen(443,'0.0.0.0');
